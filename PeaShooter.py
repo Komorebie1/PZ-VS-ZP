0c54afbb86eb3266126e181bba31d395fdb895e9
Zheng Zhixin <1160151650@qq.com>
2024-10-28 22:34:39 +0800

    大量重构

--- PeaShooter.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from Plants import Plants
-from config import GREEN, WIDTH, HEIGHT
-
-class PeaShooter(Plants):
-    def __init__(self, image_path, image_nums, position = (0, 0)):
-        super().__init__(image_path, image_nums, position)
-        self.rect.top = 100
-        self.rect.left = 250
